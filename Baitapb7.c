e9eb1cd81c87dc46bacd826fc2ad91c694a9b32b
minhman1-cyber <beanmtd@gmail.com>
2025-08-13 21:14:09 +0700

    Baitapb7
    Baitapb7

---
+++ Baitapb7.c
@@ -0,0 +1,179 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    //bai1
+    // int doDai;
+    // printf("Nhap vao do dai cua mang: ");
+    // scanf("%d", &doDai);
+    // int arr[doDai];
+    // for(int i = 0; i<doDai;i++){
+    //     printf("Nhap vao gia tri cho mang tai vi tri %d: ", i+1);
+    //     scanf("%d", &arr[i]);
+    // }
+    
+    // int dem = 0;
+    // int tong = 0;
+    // for(int i = 0; i<doDai;i++){
+    //     if(arr[i] % 2== 1){
+    //         dem++;
+    //         tong += arr[i];
+    //     }
+    // }
+    
+    // float avg = (float)tong/dem;
+    // printf("avg = %.2f", avg);
+    
+    
+    //bai2
+    // int doDai;
+    // printf("Nhap vao do dai cua mang: ");
+    // scanf("%d", &doDai);
+    // int arr[doDai];
+    // for(int i = 0; i<doDai;i++){
+    //     printf("Nhap vao gia tri cho mang tai vi tri %d: ", i+1);
+    //     scanf("%d", &arr[i]);
+    // }
+    
+    // int dem = 0;
+    // int tong = 0;
+    // for(int i = 0; i<doDai;i++){
+    //     if(i % 2==0){
+    //         if(arr[i] % 2== 1){
+    //             dem++;
+    //             tong += arr[i];
+    //         }
+    //     }
+    // }
+    
+    // float avg = (float)tong/dem;
+    // printf("avg = %.2f", avg);
+    
+    //bai3
+    // int doDai;
+    // printf("Nhap vao do dai cua mang: ");
+    // scanf("%d", &doDai);
+    // int arr[doDai];
+    // for(int i = 0; i<doDai;i++){
+    //     printf("Nhap vao gia tri cho mang tai vi tri %d: ", i+1);
+    //     scanf("%d", &arr[i]);
+    // }
+    
+    // int x;
+    // printf("Nhap vao so x: ");
+    // scanf("%d", &x);
+    
+    // int dem = 0;
+    // for(int i = 0; i<doDai;i++){
+    //     if(arr[i] == x){
+    //         dem++;
+    //     }
+        
+    // }
+    // if(dem == 0){
+    //     printf("No");
+    // }else{
+    //     printf("Yes");
+    // }
+    
+    //bai4
+    // int doDai;
+    // printf("Nhap vao do dai cua mang: ");
+    // scanf("%d", &doDai);
+    // int arr[doDai];
+    // for(int i = 0; i<doDai;i++){
+    //     printf("Nhap vao gia tri cho mang tai vi tri %d: ", i+1);
+    //     scanf("%d", &arr[i]);
+    // }
+    // int soLe = 1;
+    // for(int i = 0; i<doDai;i++){
+    //     if(arr[i] % 2 == 1){
+    //         soLe = arr[i];
+    //     }
+    // }
+    
+    // printf("so le cuoi cung: %d", soLe);
+    
+    //bai5
+    // int doDai;
+    // printf("Nhap vao do dai cua mang: ");
+    // scanf("%d", &doDai);
+    // int arr[doDai];
+    // for(int i = 0; i<doDai;i++){
+    //     printf("Nhap vao gia tri cho mang tai vi tri %d: ", i+1);
+    //     scanf("%d", &arr[i]);
+    // }
+    // int min = 2100000000;
+    // for(int i = 0; i<doDai;i++){
+    //     if(arr[i] > 0 && arr[i] < min){
+    //         min = arr[i];
+    //     }
+    // }
+    
+    // printf("so duong be nhat: %d", min);
+    
+    //bai6
+    // int doDai;
+    // printf("Nhap vao do dai cua mang: ");
+    // scanf("%d", &doDai);
+    // int arr[doDai];
+
+    // for (int i = 0; i < doDai; i++) {
+    //     printf("Nhap vao gia tri cho mang tai vi tri %d: ", i + 1);
+    //     scanf("%d", &arr[i]);
+    // }
+
+    // int maxLength = 0;
+    // int currentLength = 0;
+
+    // for (int i = 0; i < doDai; i++) {
+    //     if (arr[i] > 0) {
+    //         currentLength++;
+    //         if (currentLength > maxLength) {
+    //             maxLength = currentLength;
+    //         }
+    //     } else {
+    //         currentLength = 0; // reset khi gặp số âm hoặc 0
+    //     }
+    // }
+
+    // printf("So luong so duong lien tiep nhieu nhat la: %d\n", maxLength);
+    
+    //bai7
+    // int doDai;
+    // printf("Nhap vao do dai cua mang: ");
+    // scanf("%d", &doDai);
+    // int arr[doDai];
+
+    // for (int i = 0; i < doDai; i++) {
+    //     printf("Nhap vao gia tri cho mang tai vi tri %d: ", i + 1);
+    //     scanf("%d", &arr[i]);
+    // }
+
+    // int maxLength = 0;
+    // int currentLength = 0;
+    // int tongMax = 0;
+    // int tong = 0;
+
+    // for (int i = 0; i < doDai; i++) {
+    //     if (arr[i] > 0) {
+    //         currentLength++;
+    //         tong += arr[i];
+    //         if (currentLength > maxLength) {
+    //             maxLength = currentLength;
+    //         }
+    //         if(tong > tongMax){
+    //             tongMax = tong;
+    //         }
+    //     } else {
+    //         tong = 0;
+    //         currentLength = 0; // reset khi gặp số âm hoặc 0
+    //     }
+    // }
+
+    // printf("Tong max: %d\n", tongMax);
+    
+    
+    return 0;
+}
